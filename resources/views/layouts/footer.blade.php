63703bf6ad1ca94fd512fb3417c16891029d2a52
rafifathur <rafifathur.rahman20@gmai.com>
2024-09-28 20:27:31 +0700

    Fix : fixing rename footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,4 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; {{ date('Y') }} <a href="{{ url('/') }}">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; {{ date('Y') }} <a href="{{ url('/') }}">Asset Management Sytem</a>.</strong>
     All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.2.0
-    </div>
 </footer>
